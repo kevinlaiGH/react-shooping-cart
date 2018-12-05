4f6bf19e1111747899273b3ac4c17fb4896d28ad
Kevin Lai <kevin.km.lai@gmail.com>
2018-12-05 12:27:14 +1100

    Embedded Expression

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    const x = <h1>Zero</h1>;
+    return count === 0 ? x : count;
+  }
+}
+
+export default Counter;
